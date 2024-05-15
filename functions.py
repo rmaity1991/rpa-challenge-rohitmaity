aaed5af37e34d6ad2ca830bdb96f716ba17a8c02
Rohit Maity <official.rohitmaity@icloud.com>
2024-05-15 11:45:39 +0530

    Adding Code Changes

--- functions.py
+++ functions.py
@@ -1,7 +1,6 @@
 from RPA.Browser.Selenium import Selenium
 from RPA.Robocorp.WorkItems  import WorkItems
 import logging
-from xpaths import CONFIG
 from time import sleep
 import datetime
 from RPA.Excel.Files import Files
@@ -20,14 +19,17 @@ months={
     "November":11,
     "December":12
 }
-class RPAChallenge:
+class NewsScrapper:
 
     current_date=datetime.datetime.now()
 
-    def __init__(self,url):
+    def __init__(self,url,xpaths):
+        # Create the data url and get the current system date
         self.dataUrl=url
         self.current_date=datetime.datetime.now()
-        print(int(self.current_date.month))
+        self.browser_object=Selenium(auto_close=False)
+        self.excel_obj=Files()
+        self.xpaths=xpaths
 
     def readConfig(self):
         try:
@@ -37,7 +39,7 @@ class RPAChallenge:
             self.dataPayload=data_payload
             logging.log(logging.INFO,"Work Item data has been read succesfully")
         except Exception as e:
-            logging.log(logging.ERROR,e)
+            logging.log(logging.ERROR,f"The following error has occured whuile reading WorkItems : {e}")
             self.dataPayload=None
 
     def getDateFormat(self,date):
@@ -48,77 +50,69 @@ class RPAChallenge:
         self.browser_object.close_browser()
 
     def mainTask(self):
-        self.browser_object=Selenium(auto_close=False)
         try:
             self.browser_object.open_available_browser(url=self.dataUrl,maximized=True)
-            
             try:
-                try:
-                    self.browser_object.wait_until_page_contains_element(CONFIG["LATimes"]["search_button"],timeout=120)
-                    self.browser_object.click_element(CONFIG["LATimes"]["search_button"])
-                except Exception as e:
-                    logging.log(logging.ERROR,f'Page does contain element for {CONFIG["LATimes"]["search_button"]}')
-                    return
-                     
-                logging.log(logging.DEBUG,"The search button for the webpage is clicked")
-
-                try:
-                    self.browser_object.wait_until_page_contains_element(CONFIG["LATimes"]["search_text_field"],timeout=120)
-                    self.browser_object.input_text(CONFIG["LATimes"]["search_text_field"],self.dataPayload['SEARCH'])
-                except Exception as e:
-                    logging.log(logging.ERROR,f'Page does contain element for {CONFIG["LATimes"]["search_text_field"]}')
-                    return
-                
-                logging.log(logging.DEBUG,"The search input for the webpage is written")
-           
-                self.browser_object.click_element_when_clickable(CONFIG["LATimes"]["search_submit_button"])
-                logging.log(logging.DEBUG,"The search button for the webpage is clicked")
-
-                try:
-                    self.browser_object.wait_until_page_contains_element(CONFIG["LATimes"]["category_selection"],timeout=120)
-                except Exception as e:
-                    logging.log(logging.ERROR,f'Page does contain element for {CONFIG["LATimes"]["category_selection"]}')
-                    return
-
-                if self.browser_object.does_page_contain_element(CONFIG["LATimes"]["category_selection"]):
-                    self.browser_object.select_from_list_by_label(CONFIG["LATimes"]["category_selection"],self.dataPayload['CATEGORY'])
-                    self.browser_object.wait_until_page_contains_element(CONFIG["LATimes"]["news_results_open_status"],120)
-                    sleep(20)             
-                                                  
-                data=[]
-                if self.browser_object.is_list_selection(CONFIG["LATimes"]["category_selection"],self.dataPayload['CATEGORY']):
-                    stop_page_scroll=False
-                    while self.browser_object.does_page_contain_element(CONFIG["LATimes"]["news_next_page"]) and stop_page_scroll==False:
-                        news_title=self.browser_object.get_webelements(CONFIG["LATimes"]["news_title"])
-                        news_desc=self.browser_object.get_webelements(CONFIG["LATimes"]["news_data"])
-                        news_date=self.browser_object.get_webelements(CONFIG["LATimes"]["news_date"])
-                        
-                        for x,y,z in zip(news_title,news_desc,news_date):
-                            news_timeStamp=self.browser_object.get_text(z)
-                            news_data_split=news_timeStamp.split()
-                            month=months[news_data_split[0]]
-
-                            if  int(self.current_date.month) - int(month) > int(self.dataPayload['MONTH']):
-                                stop_page_scroll=True
-                                break
-                            data_dict={"title":self.browser_object.get_text(x),"desc":self.browser_object.get_text(y),"date_stamp":self.browser_object.get_text(z)}
-                            data.append(data_dict)
-                                            
-                        self.browser_object.scroll_element_into_view(CONFIG["LATimes"]["news_next_page"])
-                        self.browser_object.click_element(CONFIG["LATimes"]["news_next_page"])
-                        self.browser_object.wait_until_page_contains_element(CONFIG["LATimes"]["news_results_open_status"],120)
-                        sleep(15)
-                
-                excel_operations=Files()
-                workbook=excel_operations.create_workbook(path='./output/results.xlsx',sheet_name="Sheet1")
-                excel_operations.set_active_worksheet("Sheet1")
-                excel_operations.append_rows_to_worksheet(data,header=True)
-                excel_operations.save_workbook()
-                
+                self.browser_object.wait_until_page_contains_element(self.xpaths["LATimes"]["search_button"],timeout=120)
+                self.browser_object.click_element(self.xpaths["LATimes"]["search_button"])
             except Exception as e:
-                logging.log(logging.ERROR,e)
+                logging.log(logging.ERROR,f'Page does contain element for {self.xpaths["LATimes"]["search_button"]}, Try chercking the xpaths')
                 return
+                    
+            logging.log(logging.DEBUG,"The search button for the webpage is clicked")
+
+            try:
+                self.browser_object.wait_until_page_contains_element(self.xpaths["LATimes"]["search_text_field"],timeout=120)
+                self.browser_object.input_text(self.xpaths["LATimes"]["search_text_field"],self.dataPayload['SEARCH'])
+            except Exception as e:
+                logging.log(logging.ERROR,f'Page does contain element for {self.xpaths["LATimes"]["search_text_field"]}, Try chercking the xpaths')
+                return
+            
+            logging.log(logging.DEBUG,"The search input for the webpage is written")
+        
+            self.browser_object.click_element_when_clickable(self.xpaths["LATimes"]["search_submit_button"])
+            logging.log(logging.DEBUG,"The search button for the webpage is clicked")
+
+            try:
+                self.browser_object.wait_until_page_contains_element(self.xpaths["LATimes"]["category_selection"],timeout=120)
+            except Exception as e:
+                logging.log(logging.ERROR,f'Page does contain element for {self.xpaths["LATimes"]["category_selection"]}, Try chercking the xpaths')
+                return
+
+            if self.browser_object.does_page_contain_element(self.xpaths["LATimes"]["category_selection"]):
+                self.browser_object.select_from_list_by_label(self.xpaths["LATimes"]["category_selection"],self.dataPayload['CATEGORY'])
+                self.browser_object.wait_until_page_contains_element(self.xpaths["LATimes"]["news_results_open_status"],120)
+                sleep(20)             
+                                                
+            data=[]
+            if self.browser_object.is_list_selection(self.xpaths["LATimes"]["category_selection"],self.dataPayload['CATEGORY']):
+                stop_page_scroll=False
+                while self.browser_object.does_page_contain_element(self.xpaths["LATimes"]["news_next_page"]) and stop_page_scroll==False:
+                    news_title=self.browser_object.get_webelements(self.xpaths["LATimes"]["news_title"])
+                    news_desc=self.browser_object.get_webelements(self.xpaths["LATimes"]["news_data"])
+                    news_date=self.browser_object.get_webelements(self.xpaths["LATimes"]["news_date"])
+                    
+                    for x,y,z in zip(news_title,news_desc,news_date):
+                        news_timeStamp=self.browser_object.get_text(z)
+                        news_data_split=news_timeStamp.split()
+                        month=months[news_data_split[0]]
+
+                        if  int(self.current_date.month) - int(month) > int(self.dataPayload['MONTH']):
+                            stop_page_scroll=True
+                            break
+                        data_dict={"title":self.browser_object.get_text(x),"desc":self.browser_object.get_text(y),"date_stamp":self.browser_object.get_text(z)}
+                        data.append(data_dict)
+                                        
+                    self.browser_object.scroll_element_into_view(self.xpaths["LATimes"]["news_next_page"])
+                    self.browser_object.click_element(self.xpaths["LATimes"]["news_next_page"])
+                    self.browser_object.wait_until_page_contains_element(self.xpaths["LATimes"]["news_results_open_status"],120)
+                    sleep(15)
+
+            workbook=self.excel_obj.create_workbook(path='./output/results.xlsx',sheet_name="Sheet1")
+            self.excel_obj.set_active_worksheet("Sheet1")
+            self.excel_obj.append_rows_to_worksheet(data,header=True)
+            self.excel_obj.save_workbook()
         except Exception as e:
-            logging.log(logging.ERROR,e)
+            logging.log(logging.ERROR,f"The following error has occured whiole processing the bot : {e}")
             return
             
--- tasks.py
+++ tasks.py
@@ -1,5 +1,6 @@
 from robocorp.tasks import task
-from functions import RPAChallenge
+from functions import NewsScrapper
+from xpaths import CONFIG
 import logging
 
 
@@ -8,11 +9,11 @@ news_data={
 }
 @task
 def minimal_task():
-    obj=RPAChallenge(url=news_data["LATimes"])
+    obj=NewsScrapper(url=news_data["LATimes"],xpaths=CONFIG)
     data=obj.readConfig()
-
     if obj.dataPayload == None:
         logging.log(logging.DEBUG,"There are no payloads to process the bot..Exiting")
         return
     else:
+        logging.log(logging.DEBUG,"Starting Bot Operation...")
         obj.mainTask()
