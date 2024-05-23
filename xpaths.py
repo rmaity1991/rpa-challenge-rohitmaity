45bd0c6190343ed055c36a802ec453cc2637667d
Rohit Maity <official.rohitmaity@icloud.com>
2024-05-23 22:38:27 +0530

    Adding Code Changes

--- xpaths.py
+++ xpaths.py
@@ -1,16 +1,16 @@
 CONFIG={
     "LATimes":{
-        "search_button":'//button[@data-element="search-button"]',
-        "search_text_field":"//input[@data-element='search-form-input']",
-        "search_submit_button":"//button[@data-element='search-submit-button']",
-        "category_selection":"//select[@class='select-input']",
-        "news_content_count":"//div[@class='promo-content']",
-        "news_title":"//h3[@class='promo-title']",
-        "news_link":"//h3[@class='promo-title']//a[@class='link']",
-        "news_data":"//p[@class='promo-description']",
-        "news_date":"//p[@class='promo-timestamp']",
-        "news_image":"//a[@class='link promo-placeholder']",
-        "news_next_page":"//div[@class='search-results-module-next-page']",
-        "news_results_open_status":"//span[@class='search-results-module-count-desktop']",
+        "search_button":r"//button[@data-element='search-button']",
+        "search_text_field":r"//input[@data-element='search-form-input']",
+        "search_submit_button":r"//button[@data-element='search-submit-button']",
+        "category_selection":r"//select[@class='select-input']",
+        "news_content_count":r"//div[@class='promo-content']",
+        "news_title":r"//h3[@class='promo-title']",
+        "news_link":r"//h3[@class='promo-title']//a[@class='link']",
+        "news_data":r"//p[@class='promo-description']",
+        "news_date":r"//p[@class='promo-timestamp']",
+        "news_image":r"//a[@class='link promo-placeholder']",
+        "news_next_page":r"//div[@class='search-results-module-next-page']",
+        "news_results_open_status":r"//span[@class='search-results-module-count-desktop']",
     }
 }
